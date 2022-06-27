64a35f23859e6092659994871572504d83c813e7
Giuseppe-lore <loregiuseppe0@gmail.com>
2022-06-27 14:02:18 +0200

    Milestone 1 complete

--- js/script.js
+++ js/script.js
@@ -9,3 +9,35 @@
 // Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 // MILESTONE 3
 // Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+
+
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            todos: [
+                {
+                    text: 'Dare da mangiare a Sirio',
+                    done: false
+                },
+                {
+                    text: 'Portare fuori Sirio',
+                    done: false
+                },
+                {
+                    text: 'Seguire lezione Boolean',
+                    done: false
+                },
+                {
+                    text: 'Preparate pranzo',
+                    done: false
+                },
+                {
+                    text: 'Fare esercitazione Boolean',
+                    done: false
+                },
+                
+            ]
+        }
+    }
+);

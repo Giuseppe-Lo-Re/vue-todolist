94e087b0674dffa37ea6a73a434e696aa8a7dd36
Giuseppe-lore <loregiuseppe0@gmail.com>
2022-06-27 15:53:30 +0200

    working on bonus

---
+++ bonus/js/script.js
@@ -0,0 +1,46 @@
+// Bonus:
+// 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+// 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            newTodoText:'',
+            todos: [
+                {
+                    text: 'Dare da mangiare a Sirio',
+                    done: true
+                },
+                {
+                    text: 'Portare fuori Sirio',
+                    done: false
+                },
+                {
+                    text: 'Seguire lezione Boolean',
+                    done: true
+                },
+                {
+                    text: 'Preparate pranzo',
+                    done: false
+                },
+                {
+                    text: 'Fare esercitazione Boolean',
+                    done: true,
+                },
+            ]
+        },
+        methods: {
+            removeToDo(index) {
+                this.todos.splice(index, 1);
+            },
+            addNewTodoText() {
+                const newTodo = {
+                    text: this.newTodoText,
+                    done: false
+                }
+                this.todos.push(newTodo);
+            }
+        }
+    }
+);
